503d4e57e04e18ad38247221c01744ca8ec4887e
Andrey Kazlitin <akazlitin@astrata.eu>
2016-11-17 13:10:38 +0100

    Philips placeholders added

---
+++ src/C++/chapter0/Reverse.cpp
@@ -0,0 +1,4 @@
+// 1. Reverse a string without using a built-in function
+// 2. Reverse a linked list without using a built-in function
+// 3. Use standard function to reverse vector
+// 4. Implement unit test checking correctness of N1 Reverse(string) implementation
