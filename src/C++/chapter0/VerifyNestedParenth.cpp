b2b311a73a2a26c3aefe923b733e1f82cfca1efd
kerydan-home <kerydan@gmail.com>
2016-11-19 08:17:53 +0100

    VeryfyParenth

--- src/C++/chapter0/VerifyNestedParenth.cpp
+++ src/C++/chapter0/VerifyNestedParenth.cpp
@@ -1,1 +1,47 @@
 // Write Verify(text) function, checking if parentheses within text are correctly nested
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+bool VerifyParenth(const string& str) {
+
+	int par = 0;
+	for (auto i : str) {
+		if (i == '{') {
+			par--;
+		}
+		else if (i == '}') {
+			par++;
+		}
+		if (par > 0)
+			return false;
+	}
+
+	return (par == 0);
+
+}
+
+bool ParUnitTest() {
+	bool res = true;
+	if (!VerifyParenth("")
+		|| !VerifyParenth("0 {1} ")
+		|| !VerifyParenth("0 {1{}}{} ")
+		|| VerifyParenth("0 {{1{}}{} ")
+		|| VerifyParenth("0 {1{}}{}}{ ")
+		)
+	{
+		res = false;
+	}
+	if(!res)
+	{
+		cout << "TEST FAILED";
+		return false;
+	}
+	else
+	{
+		cout << "TEST PASSED";
+		return true;
+	}
+
+}
